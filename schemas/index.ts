a8e5c978a86a89120387254a706b91b74227098b
pvvquang <pvquangbg@gmail.com>
2024-08-08 14:02:14 +0700

    add prisma db

--- schemas/index.ts
+++ schemas/index.ts
@@ -4,3 +4,11 @@ export const LoginSchema = z.object({
   email: z.string().email({ message: "Email is required!" }),
   password: z.string().min(1, { message: "Password is required" }),
 });
+export type LoginFormValue = z.infer<typeof LoginSchema>;
+
+export const RegisterSchema = z.object({
+  email: z.string().email({ message: "Email is required!" }),
+  password: z.string().min(6, { message: "Minimum is 6 characters" }),
+  name: z.string().min(1, { message: "Name is required" }),
+});
+export type RegisterFormValue = z.infer<typeof RegisterSchema>;
