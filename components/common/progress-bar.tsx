8136984c665a800e2f9f4256d94c1fc383b1ed83
pvvquang <pvquangbg@gmail.com>
2024-08-15 14:55:35 +0700

    feat: add progress bar

---
+++ components/common/progress-bar.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { AppProgressBar } from "next-nprogress-bar";
+
+export default function ProgressBar() {
+  return (
+    <AppProgressBar
+      height="4px"
+      color="#fffd00"
+      options={{ showSpinner: false }}
+      shallowRouting
+    />
+  );
+}
